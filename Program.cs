656e70d25508b7a5bc84cb693794b45a5df184fe
Dudovitskiy <udovickiy1989@gmail.com>
2024-02-24 18:24:36 +0300

    Создание массива и подготовка его вывода

--- Program.cs
+++ Program.cs
@@ -2,3 +2,39 @@
 //длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
 //либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
 //лучше обойтись исключительно массивами.
+using Sistem;
+
+class Program
+{
+    static void Main(string[]args)
+    {
+        string[] inputArr = { "Hello", "2", "world", ":-)" };
+
+        string[] filteredArr = filtrdArry(inputArr);
+
+        Console.WriteLine("Новый Массив");
+        foreach (string str in filteredArr)
+        {
+            Console.WriteLine(str);
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
